c63d3d74a7f7a332d00103cf068e622d6ebccb85
andessonreis <andessonreis@gmail.com>
2023-06-14 23:34:47 -0300

    feat: add class Books

--- book/models.py
+++ book/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Books(models.Model):
+    name = models.CharField(max_length=100)
+    author = models.CharField(max_length=50)
+    co_author = models.CharField(max_length=50)
+    register_date = models.DateField()
+    borrowed = models.BooleanField(default=False)
+    name_borrowed = models.CharField(max_length=50)
+    loan_date = models.DateTimeField()
+    return_date = models.DateTimeField()
+    running_time = models.DateField()
