cc22770910292dfa1e5015c061400efc8eaa8142
andessonreis <andessonreis@gmail.com>
2023-06-15 00:17:34 -0300

    feat: add users

--- book/models.py
+++ book/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from datetime import date
 
 # Create your models here.
 
@@ -6,10 +7,16 @@ from django.db import models
 class Books(models.Model):
     name = models.CharField(max_length=100)
     author = models.CharField(max_length=50)
-    co_author = models.CharField(max_length=50)
-    register_date = models.DateField()
+    co_author = models.CharField(max_length=50, blank=True)
+    register_date = models.DateField(default=date.today)
     borrowed = models.BooleanField(default=False)
-    name_borrowed = models.CharField(max_length=50)
-    loan_date = models.DateTimeField()
-    return_date = models.DateTimeField()
-    running_time = models.DateField()
+    name_borrowed = models.CharField(max_length=50, blank=True, null=True)
+    loan_date = models.DateTimeField(blank=True, null=True)
+    return_date = models.DateTimeField(blank=True, null=True)
+    running_time = models.DateField(blank=True, null=True)
+
+    class Meta:
+        verbose_name = "Book"
+
+    def  __str__(self):
+        return self.name
