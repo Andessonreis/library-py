d199093cd1fa0f6e2e15341f3d50b429c4bbaddb
andessonreis <andessonreis@gmail.com>
2023-06-17 22:34:43 -0300

    feat: add page books

--- book/models.py
+++ book/models.py
@@ -1,8 +1,15 @@
 from django.db import models
 from datetime import date
+from users.models import User
 
 # Create your models here.
 
+class Book_category(models.Model):
+    name = models.CharField(max_length=30)
+    description = models.TextField()
+
+    def __str__(self) -> str:
+        return self.name
 
 class Books(models.Model):
     name = models.CharField(max_length=100)
@@ -14,6 +21,8 @@ class Books(models.Model):
     loan_date = models.DateTimeField(blank=True, null=True)
     return_date = models.DateTimeField(blank=True, null=True)
     running_time = models.DateField(blank=True, null=True)
+    book_category = models.ForeignKey(Book_category, on_delete=models.DO_NOTHING)
+    user = models.ForeignKey(User, on_delete=models.DO_NOTHING)
 
     class Meta:
         verbose_name = "Book"
--- book/views.py
+++ book/views.py
@@ -1,11 +1,13 @@
 from django.shortcuts import render, redirect
 from django.http import HttpResponse
 from users.models import User
+from .models import Books
 # Create your views here.
 
 def home(request):
-    if request.session.get("user"):
-        user = User.objects.get(id = request.session['user']).name
-        return HttpResponse(f"Salve de novo {user}")
+    if request.session.get('user'):
+        user = User.objects.get(id = request.session['user'])
+        books = Books.objects.filter(user = user)
+        return render(request, 'home.html', {'books': books})
     else:
         return redirect('/auth/login/?status=2')
