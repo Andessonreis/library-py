b9be488c4cd0812d67093de3c61f2d7e5f4960e3
andessonreis <andessonreis@gmail.com>
2023-06-21 19:02:05 -0300

    feat: add dander init para sobrecreve

--- book/forms.py
+++ book/forms.py
@@ -1,9 +1,11 @@
 from django import forms
 from .models import Books
 
+
 class RegisterBook(forms.ModelForm):
     class Meta: #usando para refenciar informaçoes do formulario
         model = Books
-        fields = ('name', 'author', 'co_author', 'register_date', 'borrowed', 'book_category')
+        fields = "__all__"
 
-
+    def __init__ (self, *args, **kwargs):
+        super().__init__(*args, **kwargs) ## usando super para pegar a funçao da class pai e rodar jnto/basemodelforms
