9c0859d004561da7407d80b42149410befa4639a
andessonreis <andessonreis@gmail.com>
2023-06-22 12:42:00 -0300

    criado url para detetar livros

--- book/urls.py
+++ book/urls.py
@@ -4,5 +4,6 @@ from . import views
 urlpatterns = [
     path('home/', views.home, name='home'), #url estatica
     path('book_views/<int:id>', views.book_views, name='book_views'), #url dinamica
-    path('register_book', views.register_book, name='register_book')
+    path('register_book', views.register_book, name='register_book'),
+    path('delete_book/<int:id>', views.delete_book, name='delete_book')
 ]
